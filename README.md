e2a679a5d01f7264d70095e58d98e0e4b1946711
Pablo Torres Searle <pablo@MacBook-Pro-de-Pablo.local>
2022-01-31 17:36:47 +0100

    compiler for the contract

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 This project aims to build an app like kickstarter but using blockchain technology
+
+
+
+
+//TODO
+	contract tests with ganache
+
+
---
+++ srcs/ethereum/contracts/campaing.sol
@@ -0,0 +1,86 @@
+    // SPDX-License-Identifier: GPL-3.0
+
+    pragma solidity >=0.7.0 <0.9.0;
+
+
+    contract CampaingFactory {
+        Campaing[] public campaings;
+
+        function creatCompain(uint min) public {
+            Campaing c = new Campaing(min,msg.sender);
+            campaings.push(c);
+        }
+
+        function getCampaings() public view returns (Campaing[] memory ) {
+            return campaings;
+        }
+    }
+
+    contract Campaing{
+
+
+        struct Request {
+            uint id;
+            string description;
+            uint value;
+            address  recipent; //addres the money will be sent to
+            uint    vote_yes;
+            bool    complete;
+            mapping(address => bool) approvals;
+        }
+
+        uint      private           count_requests;
+        uint      private           contributors;
+        address   public            manager;
+        uint      public            minContribution;
+        Request[] public            requests;
+        mapping(address => bool) public     approvers;
+
+        constructor(uint _min_contribution,address _manager) {
+
+            manager = _manager;
+            minContribution = _min_contribution;
+
+        }
+
+        modifier restircted() {
+            require(msg.sender == manager);
+            _;
+        }
+
+        function contribute() public payable {
+            require(msg.value >= minContribution);
+            contributors++;
+            approvers[msg.sender] = true;
+        }
+
+        function makeRequest(string memory _description, uint  _value,address  _recipent) public restircted{
+            count_requests += 1;
+            require(_value < address(this).balance);
+            Request storage r  = requests.push();
+            r.id = count_requests;
+            r.description =  _description;
+            r.value =  _value;
+            r.recipent =  _recipent;
+            r.vote_yes =  0;
+            r.complete =  false;
+        }
+
+        function aproveRequest(uint id,bool vote) public  {
+            require(approvers[msg.sender]);
+            //require(requests[id]);
+            require(!requests[id].complete);
+            require(!requests[id].approvals[msg.sender]);
+            requests[id].approvals[msg.sender] = vote;
+            if(vote)
+            {
+                requests[id].vote_yes++;
+            }
+            if(requests[id].vote_yes > contributors / 2)
+            {
+                requests[id].complete = true;
+                payable(requests[id].recipent).transfer(requests[id].value);
+            }
+        }
+
+    }
